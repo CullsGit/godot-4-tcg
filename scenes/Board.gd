2c171e9cb3c6889cffb2f9493379f8cf02434465
Hugo Cullinan <hugo.cullinan@gmail.com>
2025-01-29 23:03:32 +1100

    add ability select and deselect a card in the players hand

--- scenes/Board.gd
+++ scenes/Board.gd
@@ -3,19 +3,22 @@ extends Node2D
 var player_deck
 var opponent_deck
 
+var selected_card = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_deck = $PlayerDeck
 	opponent_deck = $OpponentDeck
-	print(player_deck.deck)
-	
+
+
 	var player_hand_node = $PlayerHand
 	for card_data in player_deck.hand:
 		var card = preload("res://scenes/Card.tscn").instantiate()
 		card.set_type(card_data["type"])
+		card.connect("gui_input", Callable(self, "_on_card_gui_input").bind(card))
 		player_hand_node.add_child(card)
-		
+
 	var opponent_hand_node = $OpponentHand
 	for card_data in opponent_deck.hand:
 		var card = preload("res://scenes/Card.tscn").instantiate()
@@ -27,6 +30,14 @@ func _ready():
 		var empty_card = preload("res://scenes/Card.tscn").instantiate()
 		slot.add_child(empty_card)
 
+func _on_card_gui_input(event, card):
+	if event is InputEventMouseButton and event.pressed:
+		print("Card clicked:", card)
+		if selected_card and selected_card != card:
+			selected_card.reset_selection()
+		selected_card = card
+		selected_card._on_card_pressed()
+		print("Selected Card Type:", selected_card.type)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- scenes/Card.gd
+++ scenes/Card.gd
@@ -1,8 +1,9 @@
 extends Panel
 
+enum CardType { RED, BLUE, GREEN, PURPLE }
 
-enum CardType { RED, BLUE, GREEN, YELLOW }
 var type: CardType
+var selected: bool = false
 
 func set_type(new_type: CardType):
 	type = new_type
@@ -10,4 +11,24 @@ func set_type(new_type: CardType):
 		CardType.RED: $ColorRect.color = Color(1, 0, 0)
 		CardType.BLUE: $ColorRect.color = Color(0, 0, 1)
 		CardType.GREEN: $ColorRect.color = Color(0, 1, 0)
-		CardType.YELLOW: $ColorRect.color = Color(0.5, 0, 1)
+		CardType.PURPLE: $ColorRect.color = Color(0.5, 0, 0.5)
+
+func _on_card_pressed():
+	selected = !selected
+	update_visual_state()
+
+func update_visual_state():
+	if selected:
+		modulate = modulate.lightened(0.3)
+	else:
+		modulate = modulate.darkened(0.3)
+
+
+func reset_selection():
+	selected = false
+	update_visual_state()
+
+func _on_card_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed:
+		print("Card detected a click!")
+		_on_card_pressed()
