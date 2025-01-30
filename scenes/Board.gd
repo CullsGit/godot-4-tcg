c97aed02c389e07e077ea1138cf58448a105080c
Hugo Cullinan <hugo.cullinan@gmail.com>
2025-01-31 10:33:46 +1100

    change the entire setup

--- scenes/Board.gd
+++ scenes/Board.gd
@@ -1,43 +1,9 @@
-extends Node2D
+extends Control
 
-var player_deck
-var opponent_deck
+var slots = []  # List of all slots on the board
 
-var selected_card = null
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	player_deck = $PlayerDeck
-	opponent_deck = $OpponentDeck
-
-
-	var player_hand_node = $PlayerHand
-	for card_data in player_deck.hand:
-		var card = preload("res://scenes/Card.tscn").instantiate()
-		card.set_type(card_data["type"])
-		card.connect("gui_input", Callable(self, "_on_card_gui_input").bind(card))
-		player_hand_node.add_child(card)
-
-	var opponent_hand_node = $OpponentHand
-	for card_data in opponent_deck.hand:
-		var card = preload("res://scenes/Card.tscn").instantiate()
-		card.set_type(card_data["type"])
-		opponent_hand_node.add_child(card)
-	
-	for i in range(9):
-		var slot = $PlayerBoard.get_child(i)
-		var empty_card = preload("res://scenes/Card.tscn").instantiate()
-		slot.add_child(empty_card)
-
-func _on_card_gui_input(event, card):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Card clicked:", card)
-		if selected_card and selected_card != card:
-			selected_card.reset_selection()
-		selected_card = card
-		selected_card._on_card_pressed()
-		print("Selected Card Type:", selected_card.type)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Find all slots inside the GridContainer
+	for child in $Grid.get_children():
+		if child is Control:
+			slots.append(child)
---
+++ scenes/Slot.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var placed_card = null  # Store the card in this slot
+
+# Function to check if the slot is empty
+func is_empty() -> bool:
+	return placed_card == null
+
+# Function to place a card in this slot
+func place_card(card):
+	if is_empty():
+		placed_card = card
+		add_child(card)  # Add card as a child of the slot
+		return true
+	return false
+
+# Function to remove a card from this slot
+func remove_card():
+	if placed_card:
+		placed_card.queue_free()  # Remove card from scene
+		placed_card = null
