f5db2dc43a9e50a9ffa819d738b5496022034110
Hugo Cullinan <hugo.cullinan@gmail.com>
2025-05-16 10:13:48 +1000

    add explicit type annotations for ActionManager.gd

--- scripts/ActionManager.gd
+++ scripts/ActionManager.gd
@@ -1,21 +1,21 @@
 extends Node
 
-@export var max_actions := 3
+@export var max_actions: int = 3
 
-var current_actions := max_actions
+var current_actions: int = max_actions
 
 signal actions_updated(current_actions)
 
 func _ready():
 	actions_updated.emit(current_actions)
 
-func use_action():
+func use_action() -> void:
 	if current_actions > 0:
 		current_actions -= 1
 		actions_updated.emit(current_actions)
 	else:
 		print("No actions left!")
 
-func reset_actions():
+func reset_actions() -> void:
 	current_actions = max_actions
 	actions_updated.emit(current_actions)
